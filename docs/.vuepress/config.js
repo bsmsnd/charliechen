417ae3b7e6c4728d75fc5f939634dd4d627cfdff
Charlie Chen <chendongcharlie@hotmail.com>
2022-06-18 16:08:11 +0800

    activate vuepress v1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,6 @@ module.exports = {
     lang: 'zh-CN',
     title: 'Charlie 的菜地',
     description: '这是我的第一个 VuePress 站点',
-    base : '/charliechen/'
+    base : '/charliechen/',
+    plugins: [ 'vuepress-plugin-latex' ]
   }
