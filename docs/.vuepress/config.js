8807b21da34402fb8e97904463c9eeb8e5975426
Charlie Chen <chendongcharlie@hotmail.com>
2022-07-03 21:43:05 +0800

    setup auto sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,9 @@ module.exports = {
     title: 'Charlie 的菜地',
     description: '这是我的第一个 VuePress 站点',
     base : '/charliechen/',
-    plugins: [ 'vuepress-plugin-latex' ]
+    plugins: [ 'vuepress-plugin-latex', 'vuepress-plugin-auto-sidebar' ],
+    themeConfig: {
+      sidebar: 'auto',
+      logo: '/logo.png',
+    }
   }
